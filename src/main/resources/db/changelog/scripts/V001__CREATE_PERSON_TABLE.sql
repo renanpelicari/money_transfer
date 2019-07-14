761229cea7872ad0e32814d8dbc3a7b00344a028
Renan Peliçari Rodrigues <renan.rodrigues@dasa.com.br>
2019-07-14 01:15:09 -0300

    configured db migration

--- src/main/resources/db/changelog/scripts/V001__CREATE_PERSON_TABLE.sql
+++ src/main/resources/db/changelog/scripts/V001__CREATE_PERSON_TABLE.sql
@@ -1,4 +1,8 @@
+--db formatted sql
+
+--changeset renanpelicari:1
 CREATE TABLE T_PERSON (
     ID SERIAL PRIMARY KEY,
     FULLNAME VARCHAR(100) NOT NULL
 )
+--rollback drop table T_PERSON;
--- src/main/resources/db/changelog/scripts/V002__CREATE_ACCOUNT_TABLE.sql
+++ src/main/resources/db/changelog/scripts/V002__CREATE_ACCOUNT_TABLE.sql
@@ -1,5 +1,10 @@
+--db formatted sql
+
+--changeset renanpelicari:2
 CREATE TABLE T_ACCOUNT (
     ID SERIAL PRIMARY KEY,
     PERSON_ID INTEGER REFERENCES T_PERSON(id),
     CURRENT_AMOUNT NUMERIC NOT NULL
 )
+--rollback drop table T_ACCOUNT;
+
